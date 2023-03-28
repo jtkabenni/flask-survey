03c505040d1208f3a3140f572361fb3ee6beb3f2
Jenny Takahara <jenny.takahara@gmail.com>
2023-03-28 00:03:25 -0400

    adding flask session

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = "secrets"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 @app.route("/")
 def display_home():
-  print (f"response length, {responses}, {len(responses)}")
-  return render_template("base.html", title = satisfaction_survey.title, instructions = satisfaction_survey.instructions, )
+  """Display home page"""
+  return render_template("base.html", survey=satisfaction_survey, )
 
 @app.route("/start", methods=["POST"])
 def start_survey():
-    responses = []
-    print (f"response length, {responses}, {len(responses)}")
-    return redirect ("/question/0")
+  """Manage start survey post request"""
+  session["responses"] = []
+  return redirect ("/question/0")
 
 @app.route("/question/<int:question_number>")
 def display_question(question_number):
-  if question_number != len(responses):
+  """Display next question in survey"""
+  responses = session["responses"]
+  if question_number != len(session["responses"]):
     flash ("Don't skip ahead!!")
-    return redirect(f"/question/{len(responses)}")
-  return render_template("question.html",title = satisfaction_survey.title, question = satisfaction_survey.questions[len(responses)], question_number = len(responses), next_question = (len(responses) +1) )
+    return redirect(f"/question/{len(responses)}") 
+  return render_template("question.html",survey=satisfaction_survey, question = satisfaction_survey.questions[len(session["responses"])])
   
-
 @app.route("/answer", methods=["POST"])
 def manage_answers():
+  """ Manage add answer post request"""
   answer = request.form.get("answer")
+  responses = session["responses"]
   if answer:
-    responses.append(answer)
-  print(responses, len(responses), len(satisfaction_survey.questions))
+    session["responses"].append(answer)
+  session["responses"] = responses
   if len(responses) == len(satisfaction_survey.questions):
     return redirect("/complete")
+
   return redirect(f"/question/{len(responses)}")
 
 @app.route("/complete")
 def say_thanks():
-  return render_template("complete.html")
+  """Display thanks/completed page after all questions are answered"""
+  return render_template("complete.html", survey=satisfaction_survey)
